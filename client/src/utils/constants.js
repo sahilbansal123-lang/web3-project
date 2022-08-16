b385b1234d5cd4395e3268dbbde717be04f5c68e
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2022-08-16 11:03:05 +0530

    initial commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x1776893d9973262154d0b18C27ceeeFc6865bA47'
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,8 @@
+import '@nomiclabs/hardhat-waffle'
+export const solidity = '0.8.0'
+export const networks = {
+  rinkeby: {
+    url: 'https://eth-rinkeby.alchemyapi.io/v2/U3X2iFUOhQfpFO75xKaUB734FASwmxnO',
+    accounts: ['b49de62fdd943b7120a8a335a0aa2f7cce34debd0b0a4b0412b5d800ab6c1774']
+  }
+}
